9e42b696cfd78b57af41992fc42807051e62fff6
luctran23 <anhvanluc1999@gmail.com>
2021-04-26 05:54:37 +0700

    add cors() module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config')
 
 const cateRoute = require('./api/routes/categories.route')
@@ -16,8 +17,9 @@ const billRoute = require('./api/routes/bills.route')
 const promotionRoute = require('./api/routes/promotions.route')
 const commentRoute = require('./api/routes/comments.route')
 const rateRoute = require('./api/routes/rates.route')
-
+app.use(cors());
 app.use(express.json());
+
 mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true }, () => {
   console.log("Connected to DB");
 })
