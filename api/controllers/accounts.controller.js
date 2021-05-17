e09c09094794b2201939da3a467a2327e0c514b1
luctran23 <anhvanluc1999@gmail.com>
2021-05-17 07:22:26 +0700

    hash password account

--- api/controllers/accounts.controller.js
+++ api/controllers/accounts.controller.js
@@ -1,4 +1,6 @@
 const Account = require("../../models/Account");
+const bcrypt = require('bcryptjs')
+const { registerValidation } = require('../../validations/validate')
 
 module.exports.getAll = async (req, res) => {
     try {
@@ -9,9 +11,12 @@ module.exports.getAll = async (req, res) => {
     }
 };
 module.exports.create = async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
     const item = new Account({
         username: req.body.username,
-        password: req.body.password
+        password: hashPassword,
+        position: req.body.position
     })
     try {
         const savedItem = await item.save();
@@ -41,11 +46,15 @@ module.exports.delete = async (req, res) => {
 
 module.exports.edit = async (req, res) => {
     try {
+
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
         const updatedItem = await Account.updateOne({ "_id": req.params.id },
             {
                 $set: {
                     username: req.body.username,
-                    password: req.body.password
+                    password: hashPassword,
+                    position: req.body.position
                 }
             })
         res.json(updatedItem);
